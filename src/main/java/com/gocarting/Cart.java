4389e037ccfbe4628ed7a7ff799ca35d60c8ef32
adargut <adar276@gmail.com>
2019-08-26 19:16:33 +0300

    Some progress

--- src/main/java/com/gocarting/Cart.java
+++ src/main/java/com/gocarting/Cart.java
@@ -1,10 +1,20 @@
-package gocarting;
+package com.gocarting;
 
+import java.util.ArrayList;
 import java.util.List;
+
+@FunctionalInterface
+interface AddCart {
+    Boolean addCart(String id);
+}
 
 public class Cart {
     // List of products inside our cart
     private List<Item> cartItems;
+
+    public Cart() {
+        this.cartItems = new ArrayList<>();
+    }
 
     /**
      * Returns the sum of all elements in {@code cartItems}.
@@ -12,14 +22,26 @@ public class Cart {
      */
     public Double getCartSum() {
         if (cartItems.size() > 0) {
-            return   cartItems.stream()
+            return  cartItems.stream()
                     .map(x -> x.getPrice())
                     .reduce(0.0, Double::sum);
         }
         return 0.0;
     }
 
-    public void addCartItem(String itemID) {
+    /**
+     * Attempts to add a new element to our shopping cart.
+     * Item is looked up using {@code itemID}.
+     */
+    public Boolean addCartItem(String itemID) {
+        AddCart a = id->{
+            Item item = ItemData.searchByItemID(id);
+            if (item == null) return false;
+            cartItems.add(item);
+            return true;
+
+        };
+        a.addCart(itemID);
         Item addedItem = ItemData.searchByItemID(itemID);
         if (addedItem == null) {
             //@TODO handle this exception!
--- src/main/java/com/gocarting/MainApplicationClass.java
+++ src/main/java/com/gocarting/MainApplicationClass.java
@@ -1,4 +1,4 @@
-package gocarting;
+package com.gocarting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApplicationClass {
 
     public static void main(String[] args) {
+        // Generate dummy item for our cart
+        for (double i = 0.0; i < 250; i++) {
+            ItemData.createNewItem(i);
+        }
+
         // Begin running...
         SpringApplication.run(MainApplicationClass.class, args);
     }
